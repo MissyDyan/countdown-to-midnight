a4d5306885e05015a7646e3666c00a2e1b110384
Diane Nguyen <diane.nguyen@gmail.com>
2021-07-21 09:43:02 -0700

    Countdown to Midnight assignment completed.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(countdown_output) 
+    countdown_output = 10  
+
+    until countdown_output == 0
+        puts "#{countdown_output} SECOND(S)!"
+        countdown_output -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+    sleep 5
 end
